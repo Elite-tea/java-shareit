1c8fe583e37160773935885fcf9269c3bcf71e00
Kirill Andreev <Andreewkirill64@yandex.ru>
2023-10-28 15:33:22 +0400

    fear: добавлена возможность комментировать предметы fear: добавлена возможность аренды(Получение, сохранение, изменение) fear: добавлен обмен с БД fix: изменена работа с хранилищами, теперь все данные проходят через репозитории

---
+++ src/main/java/ru/practicum/shareit/item/entity/Item.java
@@ -0,0 +1,44 @@
+package ru.practicum.shareit.item.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.entity.ItemRequest;
+import ru.practicum.shareit.user.entity.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Класс модель, описывающий структуру сущности <b>Item</b>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "items", schema = "public")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name_item", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "available", nullable = false)
+    @NotNull
+    private Boolean available;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private ItemRequest request;
+
+}
