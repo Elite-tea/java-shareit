f8b44d8dd026d71ee638d3ab4faf8fdc7193a991
Kirill Andreev <Andreewkirill64@yandex.ru>
2023-10-28 15:33:22 +0400

    fear: добавлена возможность комментировать предметы fear: добавлена возможность аренды(Получение, сохранение, изменение) fear: добавлен обмен с БД fix: изменена работа с хранилищами, теперь все данные проходят через репозитории

---
+++ src/main/java/ru/practicum/shareit/item/entity/Comment.java
@@ -0,0 +1,37 @@
+package ru.practicum.shareit.item.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Класс модель, описывающий структуру сущности <b>Comment</b>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comment", schema = "public")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Item item;
+
+    @Column(name = "author_name", nullable = false)
+    private String authorName;
+
+    @Column(name = "created")
+    private LocalDateTime created;
+}
